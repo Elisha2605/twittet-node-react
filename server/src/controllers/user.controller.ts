62468b341f7389e253d58ef7c632c8e7c1930648
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-04-06 22:09:54 +0200

    nothing to commit

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,8 @@
+// import { Request, Response } from 'express';
+// import asyncHandler from 'express-async-handler';
+// import { getAllUsers } from 'src/services/user.service';
+
+// export const users = asyncHandler(async (req: Request, res: Response) => {
+//     const result = await getAllUsers();
+//     res.send(result);
+// });
---
+++ server/src/routes/user.routes.ts
@@ -0,0 +1,8 @@
+// import { Router } from 'express';
+// import { users } from 'src/controllers/user.controller';
+
+// const userRouter = Router();
+
+// userRouter.get('', users);
+
+// export default userRouter;
