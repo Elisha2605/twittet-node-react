9ff307700d42991afe224c294828b25207e07779
Elisha2605 <36841977+Elisha2605@users.noreply.github.com>
2023-04-19 18:51:00 +0200

    modified seed and testData (#7)

--- server/src/config/seed.config.ts
+++ server/src/config/seed.config.ts
@@ -1,4 +1,4 @@
-import { clearDatabase, seedUsers, seedTweets } from '../tools/testData.tools';
+import { clearDatabase, seedUsers } from '../tools/testData.tools';
 import { dbClose, dbConn } from './db.config';
 import dotenv from 'dotenv';
 
@@ -17,12 +17,6 @@ const runSeeds = async (isClean: boolean, testData?: string) => {
             await seedUsers();
             console.log('Users data seeded successfully.');
         }
-
-        if (testData === 'tweets') {
-            console.log('Seeding tweets data...');
-            await seedTweets();
-            console.log('Tweets data seeded successfully.');
-        }
     } catch (error) {
         console.error('Error while seeding the database!');
         console.log(error);
@@ -33,11 +27,7 @@ const seed = async () => {
     dbConn();
     const argv = process.argv.slice(2);
     const isClean = argv.includes('clean');
-    const testData = argv.includes('users')
-        ? 'users'
-        : argv.includes('tweets')
-        ? 'tweets'
-        : undefined;
+    const testData = argv.includes('users') ? 'users' : undefined;
 
     await runSeeds(isClean, testData);
 
