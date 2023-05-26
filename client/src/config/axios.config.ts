53dc5bdf9cdf9ed3dd6b62bed832505874f3d315
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-05-26 07:51:31 +0200

    nothing to commit

--- client/src/config/axios.config.ts
+++ client/src/config/axios.config.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = process.env.NODE_ENV === 'production' ? 'https://yourapp.herokuapp.com/api' : 'http://localhost:5000/api';
+const API = process.env.NODE_ENV === 'production' ? 'https://fake-twitter.herokuapp.com/api' : 'http://localhost:5000/api';
 
 const http = axios.create({
     baseURL: API,
