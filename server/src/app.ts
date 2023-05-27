ebfb1b625472eb8a36f2d6a3cca2df9d859cce28
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-05-27 18:15:49 +0200

    nothing

--- server/src/app.ts
+++ server/src/app.ts
@@ -93,11 +93,11 @@ app.use('/api/password-reset', passwordRouter);
 
 const __dirname = path.resolve(path.dirname(''));
 
-app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-// Step 2:
-app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+// // Step 2:
+// app.get('*', function (request, response) {
+//     response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
