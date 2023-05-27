9ac3325bd507bb15314401f75d4b834c01ccfa74
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-05-27 13:33:23 +0200

    nothing

--- server/src/strategies/local.strategy.ts
+++ server/src/strategies/local.strategy.ts
@@ -1,6 +1,6 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
-import User from 'src/models/user.model';
+import User from '../../src/models/user.model';
 
 passport.use(
     new LocalStrategy(
