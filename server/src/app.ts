cd7f44ff11c6c7f692a2e7836aed7434c278ac69
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-05-27 18:15:26 +0200

    nothing

--- server/src/app.ts
+++ server/src/app.ts
@@ -95,9 +95,9 @@ const __dirname = path.resolve(path.dirname(''));
 
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 // Step 2:
-// app.get('*', function (request, response) {
-//     response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
