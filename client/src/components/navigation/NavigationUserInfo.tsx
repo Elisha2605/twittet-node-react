df5e455143d73515a0a843f0df33e5845bb2a2a9
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-05-29 10:21:43 +0200

    nothing to commit

--- client/src/components/navigation/NavigationUserInfo.tsx
+++ client/src/components/navigation/NavigationUserInfo.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import styles from './NavigationUserInfo.module.css';
 import PopUpMenu from '../ui/PopUpMenu';
-import { IMAGE_AVATAR_BASE_URL } from '../../constants/common.constants';
+import { IMAGE_AVATAR_BASE_URL, IMAGE_AVATAR_DEFAULT } from '../../constants/common.constants';
 import faLockSolid from '../../assets/faLock-solid.svg';
 
 
@@ -24,7 +24,7 @@ const NavigationUserInfo: FC<NavigationUserInfoProps> = ({
             <div className={`${styles.container}`}>
                 <div className={styles.avatar}>
                     <img src={user?.avatar ?
-                        `${IMAGE_AVATAR_BASE_URL}/${user?.avatar}` : `${IMAGE_AVATAR_BASE_URL}/${user?.avatar}`} alt="" />
+                        `${IMAGE_AVATAR_BASE_URL}/${user?.avatar}` : `${IMAGE_AVATAR_BASE_URL}/${IMAGE_AVATAR_DEFAULT}`} alt="" />
                 </div>
                 <div className={`${styles.userWrapper}`}>
                     <div className={styles.userInfo}>
