ee11cd97141e6d1abcda60207be92dced51f43f7
Elisha2605 <elisha_ngoma@yahoo.fr>
2023-07-15 01:41:45 +0200

    modified navigationUserInfo

--- client/src/components/navigation/NavigationUserInfo.tsx
+++ client/src/components/navigation/NavigationUserInfo.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import styles from './NavigationUserInfo.module.css';
 import PopUpMenu from '../ui/PopUpMenu';
-import { IMAGE_AVATAR_BASE_URL, IMAGE_AVATAR_DEFAULT } from '../../constants/common.constants';
+import {
+    IMAGE_AVATAR_BASE_URL,
+    IMAGE_AVATAR_DEFAULT,
+} from '../../constants/common.constants';
 import faLockSolid from '../../assets/faLock-solid.svg';
 import Certified from '../../assets/certified.svg';
-
 
 interface NavigationUserInfoProps {
     user: any;
@@ -17,44 +19,53 @@ const NavigationUserInfo: FC<NavigationUserInfoProps> = ({
     user,
     menuOptions,
     menuIcons,
-    onClickOption
+    onClickOption,
 }) => {
-
     return (
         <React.Fragment>
-            <div className={`${styles.container}`}>
-                <div className={styles.avatar}>
-                    <img src={user?.avatar ?
-                        `${IMAGE_AVATAR_BASE_URL}/${user?.avatar}` : `${IMAGE_AVATAR_BASE_URL}/${IMAGE_AVATAR_DEFAULT}`} alt="" />
-                </div>
-                <div className={`${styles.userWrapper}`}>
-                    <div className={styles.userInfo}>
-                        <div className={styles.name}>
-                            <p className={styles.firstname}>{user?.name}</p>
-                            {user?.isProtected && (
-                                <div className={styles.isProtected}>
-                                    <img src={faLockSolid} alt="" />
-                                </div>
-                            )}
-                            <span>
-                                {user?.isVerified && (
-                                    <img className={styles.certifiedIcon} src={Certified} alt="" />
-                                )}
-                            </span>
-                        </div>
-                        <p className={styles.username}>@{user?.username}</p>
+            <PopUpMenu
+                itemId={user?._id}
+                options={menuOptions!}
+                icons={menuIcons!}
+                isMenuIcon={false}
+                onClick={(menuOptions, id) => onClickOption!(menuOptions, id)}
+                className={styles.menuOption}
+            >
+                <div className={`${styles.container}`}>
+                    <div className={styles.avatar}>
+                        <img
+                            src={
+                                user?.avatar
+                                    ? `${IMAGE_AVATAR_BASE_URL}/${user?.avatar}`
+                                    : `${IMAGE_AVATAR_BASE_URL}/${IMAGE_AVATAR_DEFAULT}`
+                            }
+                            alt=""
+                        />
                     </div>
-                    <PopUpMenu 
-                        itemId={user?._id}
-                        options={menuOptions!}
-                        icons={menuIcons!}
-                        onClick={(menuOptions, id) => 
-                            onClickOption!(menuOptions, id)
-                        }
-                        className={styles.menuOption}
-                    />
+                    <div className={`${styles.userWrapper}`}>
+                        <div className={styles.userInfo}>
+                            <div className={styles.name}>
+                                <p className={styles.firstname}>{user?.name}</p>
+                                {user?.isProtected && (
+                                    <div className={styles.isProtected}>
+                                        <img src={faLockSolid} alt="" />
+                                    </div>
+                                )}
+                                <span>
+                                    {user?.isVerified && (
+                                        <img
+                                            className={styles.certifiedIcon}
+                                            src={Certified}
+                                            alt=""
+                                        />
+                                    )}
+                                </span>
+                            </div>
+                            <p className={styles.username}>@{user?.username}</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </PopUpMenu>
         </React.Fragment>
     );
 };
